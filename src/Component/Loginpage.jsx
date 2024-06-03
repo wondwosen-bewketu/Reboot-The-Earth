1f4896a4586ada09bebf6d52a35167506b7c219d
becki <berekeetabera@gmail.com>
2024-06-03 23:43:01 +0300

    Customized the Login Page UI

--- src/Component/Loginpage.jsx
+++ src/Component/Loginpage.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import { TEInput, TERipple } from "tw-elements-react";
 import facebookLogo from "../assets/Facebook.png";
 import googleLogo from "../assets/Google.png";
-import Logo from "../assets/logo1.png";
+import Logo from "../assets/branding.png";
 export default function LoginPage() {
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          {/* <img className="h-20" src={Logo} alt="" srcset="" /> */}
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          <img className="mx-auto w-auto h-20" src={Logo} alt="Your Company" />
+          <h2 className="mt-2 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
           </h2>
         </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
             <div>
               <label
