ab9a346efb939797eaf5951a5eb63fc0e43fbc05
becki <berekeetabera@gmail.com>
2024-06-04 01:00:35 +0300

    Customized the Admin Dashboard UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import LoginPage from "./component/Loginpage"; // Import the LoginPage component
 import LandingPage from "./component/LandingPage";
+import AdminDashboard from "./component/AdminDashboard";
 
 function App() {
   return (
     <>
-      <LoginPage />
+      {/* <LoginPage /> */}
       {/* <LandingPage /> */}
+      <AdminDashboard />
     </>
   );
 }
