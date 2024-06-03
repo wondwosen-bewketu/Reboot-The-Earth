137dc55da5329187f6ccd5926453e31c56ec2940
yohannes444 <yohannesmulat444@gmail.com>
2024-06-04 02:14:01 +0300

    select location form map

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 
 import LoginPage from "./Component/Loginpage"; // Import the LoginPage component
+import MapComponent from "./Component/selectLocation"
+import { Routes, Route } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
+import Modal from 'react-modal';
 
+// Bind modal to the app root
+Modal.setAppElement('#root');
 function App() {
   return (
     <>
-      <LoginPage /> {/* Include the LoginPage component */}
+     <BrowserRouter>
+        <div className="App">
+        <Routes>
+          <Route path="/" element={<LoginPage/>}/>
+          <Route path="/home" element={<MapComponent />}/>
+        </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
