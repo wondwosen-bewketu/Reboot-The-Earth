bc1d279bf4640b00e03a9ff12e9455af21fa09b2
Wondwosen Bewketu <wondwosen.bewketu@kegeberewtech.com>
2024-06-04 06:52:17 +0300

    fix the admin dashbaord

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,22 @@ import LoginPage from "./Component/Loginpage"; // Import the LoginPage component
 import LandingPage from "./Component/Landingpage";
 import AdminDashboard from "./Component/AdminDashboard";
 import FarmerPage from "./Component/FarmerPage";
-import selectLocation from "./Component/selectLocation";
+import SelectLocation from "./Component/selectLocation";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    
     <Router>
       {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/login" element={<AdminDashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/farmer" element={<FarmerPage />} />
-        <Route path="/govt" element={<selectLocation />} />
-
-
+        <Route path="/govt" element={<SelectLocation />} />
       </Routes>
     </Router>
-
   );
 }
 
